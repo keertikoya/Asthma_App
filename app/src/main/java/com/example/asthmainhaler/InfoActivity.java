b199732d901607e15062c9f222065ec8c9430613
Keerti Koya <koyakeerti@gmail.com>
2024-03-04 21:24:40 -0800

    arduino connectivity

--- app/src/main/java/com/example/asthmainhaler/InfoActivity.java
+++ app/src/main/java/com/example/asthmainhaler/InfoActivity.java
@@ -5,13 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class InfoActivity extends AppCompatActivity {
 
-    ImageButton userButton;
-    ImageButton homeButton;
-    ImageButton infoButton;
+    ImageButton userButton, homeButton, infoButton;
+    Button goButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
